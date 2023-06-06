b1a8f8f07dd351f21613423d60a1eae18733526a
AndreCbrera <den282f@gmail.com>
2023-06-07 00:17:25 +0200

    print globl variable in scope labs

--- scopes/parametersvariables.go
+++ scopes/parametersvariables.go
@@ -5,19 +5,28 @@ import "fmt"
 /* Declare global variable */
 var a int = 20
 
-func main()  {
-	/* main() local variables */
-	var a int = 10
-	var b int = 20
-	var c int = 0
+func main() {
 
-	fmt.Printf("main a = %d\n", a);
-	c = sum(a,b);
-	fmt.Printf("main c = %d\n", c);
+	globalVar(a)
+	
+    /* main() local variables */
+    var a int = 10
+    var b int = 20
+    var c int = 0
+
+    fmt.Printf("main a = %d\n", a)
+    c = sum(a, b)
+    fmt.Printf("main c = %d\n", c)
+
+
 }
 
 func sum(a, b int) int {
-	fmt.Printf("sum() int the function a = %d\n", a)
-	fmt.Printf("sum() int the function b = %d\n", b)
-	return a + b
+    fmt.Printf("sum() int the function a = %d\n", a)
+    fmt.Printf("sum() int the function b = %d\n", b)
+    return a + b
+}
+
+func globalVar(a int) {
+    fmt.Printf("Hi, I'm a global variable: a = %d\n", a)
 }
